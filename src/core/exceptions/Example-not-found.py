1d9be1cc1a87764c427a0f312ae83de2c368888f
Silx79 <javier.mira46@gmail.com>
2025-02-20 19:32:31 +0100

    delete

--- src/core/exceptions/Example-not-found.py
+++
@@ -1,7 +0,0 @@
-class ExampleNotFoundError(Exception):
-    """
-    Excepción personalizada para manejar el caso donde no se encuentra un ejemplo.
-    """
-    def __init__(self, example_id):
-        super().__init__(f"Example with ID {example_id} not found.")
-        self.example_id = example_id
