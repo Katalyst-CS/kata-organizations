f37f5e188a5f0fc27f2fe97a3558805195dcf54f
Silx79 <javier.mira46@gmail.com>
2025-02-20 19:44:26 +0100

    update

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from src.infrastructure.http.controllers.organization_controller import organization_bp
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(organization_bp)
+
+    @app.route('/')
+    def index():
+        return "API Kata organizations corriendo."
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
